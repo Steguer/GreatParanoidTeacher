7025df6c058bb9d11c883943188beb6dae1e02ef
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-02-27 23:02:01 -0500

    Merge master

---
+++ Assets/Scripts/Student.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Student : MonoBehaviour {
+
+	
+	private Animator animator;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animator> ();
+	}
+
+	public void Cheat()
+	{
+		animator.SetTrigger ("isCheating");
+	}
+
+	public void Hit()
+	{
+		animator.SetTrigger ("isHit");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
