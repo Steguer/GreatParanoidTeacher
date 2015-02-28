ee91c3a4e482e0820593519a315e0deddb57c212
corentin-R <corentinraoult@laposte.net>
2015-02-28 19:12:34 +0100

    hand début

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	//********* Public attributs *********
 	public Texture2D cursorTexture;
 	public float fireRate = 0.5F;
 	public int stressLimit = 10;
 	public int playerStress = 0;
 	public GameObject objetCraie;
-
+	
 	//********* Private attributs *********
 	private float nextFire = 0.0F;
 	private CursorMode cursorMode = CursorMode.Auto;
 	private Vector2 hotSpot;
 	private Vector3 pos;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		var tmp = new Vector2(cursorTexture.width/2, cursorTexture.height/2);
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		var hit = fire ();
 		checkHitState(hit);
-
+		
 		var mousePos = Input.mousePosition;
 		mousePos.z = 10; // select distance = 10 units from the camera
 		pos = GameObject.Find("Main Camera").camera.ScreenToWorldPoint(mousePos);
@@ -38,9 +38,9 @@ public class PlayerController : MonoBehaviour {
 		//pos.z = 20f;
 		//pos = Camera.main.ScreenToWorldPoint(pos);
 		//objetCraie.GetComponent<moveCraie> ().arrivee = pos;
-
+		
 	}
-
+	
 	/** 
 	 * Manage when player fire
 	 * 
@@ -50,27 +50,28 @@ public class PlayerController : MonoBehaviour {
 		RaycastHit2D hit;
 		// Check if we can fire
 		if (Input.GetButton("Fire1") && Time.time >= nextFire) {
-
-
-
+			GameObject.Find ("SoundMaker").GetComponent<SoundMaker>().playArmSwing();
+			
+			
 			nextFire = Time.time + fireRate;
 			// Ray tracing
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
-
+			
 			if(hit.collider != null) {
 				// Check if hit a student
 				if(hit.transform.tag == "Student") {
 					instanciateCraie(hit.transform);
-
+					
 					return hit.transform;
 				}
 			}
 			instanciateCraie(null);
+			
 		}
 		return null;
 	}
-
+	
 	/**
 	 * Check the state of the hit. Increase stress if it's in StudentIdle state, decrease if it's in StudentCheating state.
 	 */
@@ -86,13 +87,13 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	private void instanciateCraie(Transform hit) {		
 		GameObject cr = Instantiate (objetCraie, Input.mousePosition, Quaternion.identity) as GameObject; 
 		cr.GetComponent<moveCraie> ().arrivee = new Vector3(pos.x, pos.y, pos.z);
 		Vector3 arm = GameObject.Find ("Main Camera").camera.transform.position;
 		cr.GetComponent<moveCraie> ().depart = new Vector3(arm.x+0.4f, arm.y-0.4f,arm.z);
-
+		
 		if(hit != null) {
 			cr.GetComponent<moveCraie>().studentTargeted = hit.gameObject;
 		}
