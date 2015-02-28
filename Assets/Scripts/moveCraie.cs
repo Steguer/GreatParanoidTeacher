41d48a61fe4dc0ce28660746361d8cdf8965f761
corentin-R <corentinraoult@laposte.net>
2015-02-28 13:44:16 +0100

    refactore craie

--- Assets/Scripts/moveCraie.cs
+++ Assets/Scripts/moveCraie.cs
@@ -2,37 +2,35 @@
 using System.Collections;
 
 public class moveCraie : MonoBehaviour {
-
-	public float x_depart;
-	public float y_depart;
-
-	public float x_arrivee;
-	public float y_arrivee;
-
+	
+	public Vector3 depart;
+	public Vector3 arrivee;
 	public GameObject objet;
-
+	
 	private float dMin = 0.10f;
-
 	private float speedCarre;
 	private float speedX;
 	private float speedY;
-
 	private float scaleDiff;
-
+	
 	// Use this for initialization
 	void Start () {
 		speedCarre = 0.04f;
-		speedX = (x_arrivee - x_depart)*speedCarre; 
-		speedY = (y_arrivee - y_depart)*speedCarre;
-
-		objet.transform.position = (new Vector3(x_depart, y_depart, 1f));//;
+		speedX = (arrivee.x - depart.x)*speedCarre; 
+		speedY = (arrivee.y - depart.y)*speedCarre;
+		
+		Vector3 screenPoint =new Vector3 (250f, 250f, 20f);
+		var worldPos = GameObject.Find("Main Camera").camera.ScreenToWorldPoint ( screenPoint );
+		//var obj = Instantiate( objet , worldPos, Quaternion.identity );
+		
+		objet.transform.position = worldPos;
 		scaleDiff = speedCarre * 0.8f;
-
+		Debug.Log (depart);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float d = Mathf.Pow (objet.transform.position.x -x_arrivee, 2) + Mathf.Pow (objet.transform.position.y-y_arrivee, 2);
+		float d = Mathf.Pow (objet.transform.position.x -arrivee.x, 2) + Mathf.Pow (objet.transform.position.y-arrivee.y, 2);
 		//Debug.Log (d);
 		if (d < dMin) {
 			Destroy(objet);
