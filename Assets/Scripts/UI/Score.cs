ec0a973a95aea7199b29d2fb56d0d9f2969864c9
yannismrad <yannismrad@free.fr>
2015-02-28 21:35:29 -0500

    Affichage timer & score + responsive design^^

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Text> ().text = player.GetComponent<PlayerController> ().score + "";
+	}
+}
