7568bab870e0b4f9d50e4e72909c6d24fe57dc5f
corentin-R <corentinraoult@laposte.net>
2015-03-01 13:27:07 +0100

    bonus/malus

---
+++ Assets/Scripts/points.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class points : MonoBehaviour {
+
+	public Vector3 posDepart;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0,.04f,0));
+	}
+}
