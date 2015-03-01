f39e7175673509cbf885f5e7fde6c3aa6f456200
yannismrad <yannismrad@free.fr>
2015-03-01 09:46:46 -0500

    Responsive design ecran score + fix main menu

---
+++ Assets/Scripts/UI/EndGame/Endmenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Endmenu : MonoBehaviour {
+	private Button rButton, qButton;
+	private AudioSource clickS;
+	public AudioClip clickSound;
+	private string playLevel = "test_intégration_1", returnMenu = "test_mainMenu";
+
+	// Use this for initialization
+	void Start () {
+		clickS = gameObject.AddComponent<AudioSource> ();
+		clickS.clip = clickSound;
+
+		rButton = GameObject.Find ("Btn_Restart").GetComponent<Button> ();
+		qButton = GameObject.Find ("Btn_Quit").GetComponent<Button> ();
+
+		rButton.onClick.AddListener (rButtonListener);
+		qButton.onClick.AddListener (qButtonListener);
+
+	
+	}
+
+	/**
+	 * Listener bouton retry
+	 * */
+	public void rButtonListener(){
+		clickS.Play ();
+		Application.LoadLevel (playLevel);
+	}
+	
+	/**
+	 * Listener bouton quit (retourner au menu principal)
+	 * */
+	public void qButtonListener(){
+		clickS.Play ();
+		Application.LoadLevel (returnMenu);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
