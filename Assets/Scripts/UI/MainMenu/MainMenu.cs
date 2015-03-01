029bcc260d04648e0b4407970e559ef1f7f90913
yannismrad <yannismrad@free.fr>
2015-02-28 23:08:29 -0500

    Main Menu + ressources

---
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	private Button pButton, qButton, sButton, tButton;
+	private string playLevel = "test_intégration_1", scoreLevel="test_score", tutoLevel ="test_tuto";
+	public AudioClip clickSound, hoverSound, music;
+	private AudioSource clickS, hoverS, menuMusic;
+
+	// Use this for initialization
+	void Start () {
+
+		clickS = gameObject.AddComponent<AudioSource> ();
+		clickS.clip = clickSound;
+
+		hoverS = gameObject.AddComponent<AudioSource> ();
+		hoverS.clip = hoverSound;
+
+		menuMusic = gameObject.AddComponent<AudioSource> ();
+		menuMusic.clip = music;
+		menuMusic.volume = 0.7f;
+		menuMusic.Play ();
+
+		pButton = GameObject.Find ("Btn_Play").GetComponent<Button> ();
+		qButton = GameObject.Find ("Btn_Quit").GetComponent<Button> ();
+		sButton = GameObject.Find ("Btn_Score").GetComponent<Button> ();
+		tButton = GameObject.Find ("Btn_Tuto").GetComponent<Button> ();
+
+
+		pButton.onClick.AddListener (pButtonListener);
+		qButton.onClick.AddListener (qButtonListener);
+		sButton.onClick.AddListener (sButtonListener);
+		tButton.onClick.AddListener (tButtonListener);
+	}
+
+	/**
+	 * Listener bouton play
+	 * */
+	public void pButtonListener(){
+		clickS.Play ();
+		Application.LoadLevel (playLevel);
+	}
+
+	/**
+	 * Listener bouton quit
+	 * */
+	public void qButtonListener(){
+		clickS.Play ();
+		Application.Quit ();
+	}
+
+	/**
+	 * Listener bouton score
+	 * */
+	public void sButtonListener(){
+		clickS.Play ();
+		//Application.LoadLevel (scoreLevel);
+	}
+
+	/**
+	 * Listener bouton tuto
+	 * */
+	public void tButtonListener(){
+		clickS.Play ();
+		//Application.LoadLevel (tutoLevel);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
